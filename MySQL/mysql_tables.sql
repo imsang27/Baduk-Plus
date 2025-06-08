1c2a2b0358799506466ba9f46750a180213f0b6c
imsang27 <imsang27@naver.com>
2025-06-09 01:35:24 +0900

    fix: game_results 테이블의 result 컬럼 타입을 TEXT로 변경
    - 대국 결과 테이블의 result 컬럼을 VARCHAR(50)에서 TEXT로 수정하여 더 긴 결과를 저장할 수 있도록 개선함

--- MySQL/mysql_tables.sql
+++ MySQL/mysql_tables.sql
@@ -1,6 +1,6 @@
 -- 대국 결과 테이블
 CREATE TABLE game_results (
     game_id VARCHAR(255) PRIMARY KEY,
-    result VARCHAR(50) NOT NULL,
+    result TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
